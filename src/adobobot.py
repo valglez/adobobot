418997837ff62e81abf9f3741d19832d3ab914ff
Valglez <71898653+valglez@users.noreply.github.com>
2022-08-11 01:41:41 +0200

    feature: add mongodb (#1)
    * migration to mongodb
    
    * migrate to mongodb
    
    * fix metrics
    
    * mod defs
    
    * mod defs
    
    * stable version

--- src/adobobot.py
+++ src/adobobot.py
@@ -1,56 +1,42 @@
 from datetime import datetime
-import mysql.connector
+import pymongo
 import telebot
 import os
 
 # Inicializar bot
 bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
 
-# Conexión con el servidor MySQL Server
-conn= mysql.connector.connect(
-    host=os.environ.get('DB_HOST'),
-    user=os.environ.get('DB_USER'),
-    passwd=os.environ.get('DB_PASSWD'),
-    db=os.environ.get('DB_DBNAME')
-)
+# Conexion con MongoDB
+conn = pymongo.MongoClient(os.environ.get('DB_CONN'))
+db = conn[os.environ.get('DB_DBNAME')]
+coll = db[os.environ.get('DB_COLL')]
 
 # Definición de métodos
 def get_chatid(message):
 	return message.chat.id
 
+def count_chats_for_user(chat_id, user_id):
+    return coll.count_documents({"ChatID": chat_id, "UserID": user_id})
+
+def get_username_by_id(chat_id, user_id):
+    result = coll.find({"ChatID": chat_id, "UserID": user_id}).limit(1)
+    return result[0]["Username"] or "Anonymous"
+
 def get_metrics_by_chat(message):
-	cursor = conn.cursor()
-	cursor.execute(f"SELECT Username, COUNT(UserID) FROM chat_log WHERE ChatID = {get_chatid(message)} GROUP BY UserID")
-	result = cursor.fetchall()
-	response = ""
-	for row in result:
-		user = row[0] or "Anonymous"
-		response += "El usuario " + user + " ha enviado " + str(row[1]) + " mensajes. \n"
-	return response
-
-def exec_query(query):
-	cursor = conn.cursor()
-	cursor.execute(query)
-	result=cursor.fetchall()
-	return result
-
-def get_top_user_by_chat(message):
-	result = exec_query(f"SELECT Username, COUNT(*) AS Total FROM chat_log WHERE ChatID = {get_chatid(message)} GROUP BY Username ORDER BY Total DESC LIMIT 1")
-	response=""
-	for row in result:
-		user = row[0] or "Anonymous"
-		response += "El usuario " + user + " ha sido el usuario más activo con un total de " + str(row[1]) + " mensajes. \n"
-	return response
+    users_id = coll.distinct("UserID", {"ChatID": get_chatid(message)})
+    response = ""
+    for id in users_id:
+        usernames = get_username_by_id(get_chatid(message), id)
+        users_logs = str(count_chats_for_user(get_chatid(message), id))
+        response += "El usuario " + usernames + " ha enviado " + users_logs + " mensajes. \n"
+    return response
 
 def insert_message_query(message):
-	cursor = conn.cursor()
-	query = "INSERT INTO chat_log (UserID, Username, Date, ChatID, Text) VALUES (%s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE UserID=VALUES(UserID), Username=VALUES(Username), Date=VALUES(Date), ChatID=VALUES(ChatID), Text=VALUES(Text)"
-	ts=(datetime.utcfromtimestamp(message.date).strftime('%Y-%m-%d %H:%M:%S'))
-	values = (message.from_user.id, message.from_user.username, ts, message.chat.id, message.text)
-	cursor.execute(query, values)
-	conn.commit()
+    ts = (datetime.utcfromtimestamp(message.date).strftime('%Y-%m-%d %H:%M:%S'))
+    query = [{"UserID": message.from_user.id, "Username": message.from_user.username, "Date": ts, "ChatID": message.chat.id, "Text": message.text}]
+    coll.insert_many(query)
 
-# Definición de handlers
+#Definición de handlers
 @bot.message_handler(commands=['start'])
 def send_start(message):
 	bot.reply_to(message, 'Hola, mi nombre es adobobot. Escribe /help para mostrarte los comandos disponibles.')
@@ -63,16 +49,20 @@ def send_help(message):
 def about_bot(message):
 	bot.reply_to(message, 'Developed by valglez @ https://github.com/valglez')
 
-@bot.message_handler(commands=['top_user'])
-def top_user(message):
-	bot.reply_to(message, get_top_user_by_chat(message))
-
 @bot.message_handler(commands=['metrics'])
-def metric_users(message):
-  bot.reply_to(message, get_metrics_by_chat(message))
+def users_metrics(message):
+    bot.reply_to(message, get_metrics_by_chat(message))
 
 @bot.message_handler(content_types=['text'])
-def store_chat(message):
+def store_messages(message):
 	insert_message_query(message)
+
+# TO DO
+# This handler doesn't work cause needs a definition to obtain the metric's output
+# in dict format
+
+@bot.message_handler(commands=['top_user'])
+def users_metrics(message):
+    bot.reply_to(message, get_metrics_by_chat(message))
 
 bot.infinity_polling()
